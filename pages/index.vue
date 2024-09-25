904102feda05184a25deec041a2fdaa4fb287ad5
Ega Ananta <egaananta2004@.com>
2024-09-25 23:34:05 +0700

    second commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,43 @@
 <template>
   <div class="flex min-h-screen flex-col">
     <!-- Header Section -->
-      <header class="hero min-h-1/4 bg-cover bg-bottom xl:bg-bottom">
-        <div class="pt-0 md:pt-2"></div>
-        <nav
-          class="mx-auto w-full rounded-lg bg-primary bg-opacity-30 p-4 shadow-blue-500 backdrop-blur-sm transition-shadow duration-300 hover:shadow-lg hover:shadow-blue-600 md:w-4/5"
+    <header class="hero min-h-1/4 bg-cover bg-bottom xl:bg-bottom">
+      <div class="pt-0 md:pt-2"></div>
+      <nav
+        class="mx-auto w-full rounded-lg bg-primary bg-opacity-30 p-4 shadow-blue-500 backdrop-blur-sm transition-shadow duration-300 hover:shadow-lg hover:shadow-blue-600 md:w-4/5"
+      >
+        <ul
+          class="flex items-center justify-around gap-x-8 text-lg font-bold text-white"
         >
-          <ul
-            class="flex items-center justify-around gap-x-8 text-lg font-bold text-white"
+          <li
+            class="cursor-pointer transition-colors duration-300 hover:text-yellow-300"
           >
-            <li
-              class="cursor-pointer transition-colors duration-300 hover:text-yellow-300"
-            >
-              Home
-            </li>
-            <span class="hidden h-8 border-r-2 border-yellow-100 md:block"></span>
-            <li
-              class="cursor-pointer transition-colors duration-300 hover:text-yellow-300"
-            >
-              Radar
-            </li>
-            <span class="hidden h-8 border-r-2 border-yellow-100 md:block"></span>
-            <li
-              class="cursor-pointer transition-colors duration-300 hover:text-yellow-300"
-            >
-              Blog
-            </li>
-            <span class="hidden h-8 border-r-2 border-yellow-100 md:block"></span>
-            <li
-              class="cursor-pointer transition-colors duration-300 hover:text-yellow-300"
-            >
-              Kontak
-            </li>
-          </ul>
-        </nav>
-        <h2 class="tracking-white px-10 pt-20 text-9xl font-bold text-yellow-100">
-          Hello!
-        </h2>
-      </header>
+            Home
+          </li>
+          <span class="hidden h-8 border-r-2 border-yellow-100 md:block"></span>
+          <li
+            class="cursor-pointer transition-colors duration-300 hover:text-yellow-300"
+          >
+            Radar
+          </li>
+          <span class="hidden h-8 border-r-2 border-yellow-100 md:block"></span>
+          <li
+            class="cursor-pointer transition-colors duration-300 hover:text-yellow-300"
+          >
+            Blog
+          </li>
+          <span class="hidden h-8 border-r-2 border-yellow-100 md:block"></span>
+          <li
+            class="cursor-pointer transition-colors duration-300 hover:text-yellow-300"
+          >
+            Kontak
+          </li>
+        </ul>
+      </nav>
+      <h2 class="tracking-white px-10 pt-20 text-9xl font-bold text-yellow-100">
+        Hello!
+      </h2>
+    </header>
 
     <!-- Main Content -->
     <main class="flex-grow">
@@ -45,20 +45,15 @@
         <h2 class="text-4xl">
           Namaku Ega<br />
           - <br />
-          mahasiswa di universitas brawijaya
+          Mahasiswa Teknologi Informasi di Universitas Brawijaya
         </h2>
         <h1 class="py-12 text-5xl">
-          "Kejarlah
+          Front End Web
           <span
             class="bg-gradient-to-r from-yellow-100 to-blue-900 bg-clip-text font-extrabold tracking-wide text-transparent"
           >
-            ilmu
+            Developer
           </span>
-          sampai ke negeri
-          <span
-            class="bg-gradient-to-r from-yellow-100 to-blue-900 bg-clip-text font-extrabold tracking-wide text-transparent"
-            >cina</span
-          >"
         </h1>
       </section>
     </main>
@@ -67,7 +62,15 @@
     <footer
       class="flex items-center border-t-2 border-gray-600 bg-gray-100 p-4 text-gray-600"
     >
-      <img class="w-16" src="/assets/images/html.png" alt="HTML" />
+      <h2
+        class="cursor-pointer font-bold transition-colors duration-300 hover:text-yellow-300"
+      >
+        My Skills :
+      </h2>
+      <img class="mr-2 w-16" src="/assets/images/2.png" alt="HTML" />
+      <img class="mr-2 w-16" src="/assets/images/3.png" alt="CSS" />
+      <img class="mr-2 w-16" src="/assets/images/7.png" alt="JavaScript" />
+      <img class="mr-2 w-16" src="/assets/images/6.png" alt="PHP" />
       <p class="ml-4 text-sm">
         © 2024 CV -
         <a
@@ -77,19 +80,34 @@
           >@MyCV</a
         >
       </p>
-      <div class="ml-auto flex space-x-4">
-        <a href="" target="_blank">
+        <h2
+          class="ml-80 cursor-pointer font-bold transition-colors duration-300 hover:text-yellow-300"
+        >
+          Social Media :
+        </h2>
+      <div class="ml-auto mr-20 flex space-x-6">
+        <a href="https://github.com/Egaa25" target="_blank">
           <img
-            class="w-8 transition-opacity duration-300 hover:opacity-80"
-            src="/assets/images/github.png"
+            class="w-12 transition-opacity duration-300 hover:opacity-80"
+            src="/assets/images/4.png"
             alt="GitHub"
           />
         </a>
-        <a href="" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/chrisnanda-ega-ananta-0b0ab3288/"
+          target="_blank"
+        >
           <img
-            class="w-8 transition-opacity duration-300 hover:opacity-80"
-            src="/assets/images/linkedin.png"
+            class="w-12 transition-opacity duration-300 hover:opacity-80"
+            src="/assets/images/5.png"
             alt="LinkedIn"
+          />
+        </a>
+        <a href="https://www.instagram.com/chrisnandaegaa_/" target="_blank">
+          <img
+            class="w-12 transition-opacity duration-300 hover:opacity-80"
+            src="/assets/images/1.png"
+            alt="Instagram"
           />
         </a>
       </div>
